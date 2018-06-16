82413ecba5873706601b61b101eba75f987e6d7f
arulmozhi <arul.natarajan@transavia.com>
2018-06-16 23:42:16 +0200

    update filters

--- src/App.js
+++ src/App.js
@@ -13,12 +13,28 @@ class App extends Component {
         fetch('Api/data.json').then((data) => {
             return data.json()
         }).then((json) => {
-            this.setState({post:json})
+            this.setState({post:json,filteredPosts:json})
         })
     }
 
+    findHandel = (event) => {
+        const value = event.target.value.toLowerCase();
+        let relevantHandles = [];
+        if(value.indexOf('@') !== -1) {
+            relevantHandles = this.state.post.filter(x => x.user.screen_name.toLowerCase().includes(value.replace('@','')))
+        }else{
+            relevantHandles = this.state.post.filter(x => x.user.name.toLowerCase().includes(value))
+        }
+        this.setState({filteredPosts:relevantHandles})
+    }
+
     showRetweetsOnly = () => {
-        console.log()
+        const retweetedTweets = this.state.post.filter(x => x.retweeted_status)
+       this.setState({filteredPosts:retweetedTweets})
+    }
+
+    showAllTweets = () => {
+        this.setState({filteredPosts:this.state.post})
     }
 
     render() {
@@ -26,13 +42,13 @@ class App extends Component {
       <div className="container">
           <div className="leftPanel">
               <h3>Tweet Filters</h3>
-              <div className="searchBox"> <input placeholder="@name-handel"/></div>
+              <div className="searchBox"> <input placeholder="@nameHandel or name" onChange={this.findHandel}/></div>
               <h4>Show only</h4>
-              <div className="radioButton"><input type="radio" id="contactChoice2" name="contact" value="phone" /><label htmlFor="contactChoice2">All tweets</label></div>
-              <div className="radioButton"><input type="radio" id="contactChoice1" name="contact" value="email" /><label htmlFor="contactChoice1">Retweeted tweets</label></div>
+              <div className="radioButton"><input type="radio" id="filterChoice2" name="filter" value="showall" onClick={this.showAllTweets} defaultChecked={true}/><label >All tweets</label></div>
+              <div className="radioButton"><input type="radio" id="filterChoice1" name="filter" value="filter" onClick={this.showRetweetsOnly}/><label htmlFor="contactChoice1">Retweeted tweets</label></div>
           </div>
           <div className="content">
-          {this.state.post.map(post => (
+          {this.state.filteredPosts.map(post => (
               <PostCard text={post.text} key={post.id} user={post.user} userMentioned={post.entities.user_mentions}/>
           ))}
           </div>

55e315d27917419c6f67f563f796a8bb1d8bd6e8
arulmozhi <arul.natarajan@transavia.com>
2018-06-17 02:45:29 +0200

    add style to retweet

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -5,7 +5,7 @@ class PostCard extends React.Component{
 
     render(){
         return (
-            <div className={this.props.retweet ? 'postCard retweet':'postCard'}>
+            <div className={this.props.retweet ? 'postCard retweetPost':'postCard'}>
                 <TextParser text={this.props.text} user={this.props.user} userMentioned={this.props.userMentioned} />
             </div>
         )
